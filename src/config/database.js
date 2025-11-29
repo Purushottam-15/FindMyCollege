9e0ad1b1231e4e1d0f2340ce18addf72dd5209bd
Purushottam <1032241753@tcetmumbai.in>
2025-11-29 22:38:17 +0530

    commit5

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
     try {
-        const conn = await mongoose.connect(`${process.env.MONGO_URL}/college_search`);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error('Database connection error:', error);
