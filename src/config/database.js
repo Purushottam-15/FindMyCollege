3b22334be331a903c2022dc076f4b33b44c28214
Purushottam <1032241753@tcetmumbai.in>
2025-12-06 18:22:25 +0530

    database.js variable change

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const LOCAL_MONGODB_URI = 'mongodb://localhost:27017/college_search';
 const connectDatabase = async () => {
     try {
-        const conn = await mongoose.connect(LOCAL_MONGODB_URI);
-        console.log(`MongoDB Connected Locally: ${conn.connection.host}`);
+        // const conn = await mongoose.connect(LOCAL_MONGODB_URI);
+        const uri = process.env.MONGO_URL || LOCAL_MONGODB_URI;
+        const conn = await mongoose.connect(uri);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error('Database connection error:', error);
-        console.error('*** HINT: Is your local MongoDB server running? ***');
         process.exit(1);
     }
 };
