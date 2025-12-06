16eee44ec3d73b7cc647b60917f5350a71115682
Purushottam <1032241753@tcetmumbai.in>
2025-12-06 19:25:21 +0530

    database.js variable change3

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const LOCAL_MONGODB_URI = 'mongodb://localhost:27017/findmycollege';
+
 const connectDatabase = async () => {
     try {
-        // const conn = await mongoose.connect(LOCAL_MONGODB_URI);
         const uri = process.env.MONGO_URL || LOCAL_MONGODB_URI;
         const conn = await mongoose.connect(uri);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
